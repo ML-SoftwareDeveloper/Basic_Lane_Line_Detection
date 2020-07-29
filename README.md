6ca483ffecdf1da90db728af4dbdc937c74b9f6b
Ramkumar Sakthivel <ramkumarsakthivel93@gmail.com>
2020-07-29 16:58:25 +0200

    add pipeline

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
 # Basic_Lane_Line_Detection
 Detection of lane lines using basic computer vision techniques.
 
-Steps:
-1. Read input image
-2. Covert to grayscale
-3. Apply gaussian blur
-4. Apply canny edge detection
-5. Apply polygonal mask
-6. Detect hough lines
-7. Segregate points belonging to left and right lane lines
-8. Fit left and right lines using regression
-9. Draw lane lines
-10. Display/save the image
+Output:
+![](output/out.gif)
--- lane_detection.py
+++ lane_detection.py
@@ -3,79 +3,132 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+# Lane line parameters
+avg_left_line_slope = 1
+avg_left_line_intercept = 1
+avg_right_line_slope = 1
+avg_right_line_intercept = 1
 
-# Read image
-image = mpimg.imread('images/test2.jpg')
-y_size = image.shape[0]
-x_size = image.shape[1]
-img_copy = np.copy(image)
+def lane_line_detection(frame):
+    # Set global variables
+    global avg_left_line_slope, avg_left_line_intercept, avg_right_line_slope, avg_right_line_intercept
+    # Read image
+    #image = mpimg.imread('images/test2.jpg')
+    image = frame
+    y_size = image.shape[0]
+    x_size = image.shape[1]
+    img_copy = np.copy(image)
 
-# Grayscale
-gray = cv2.cvtColor(img_copy,cv2.COLOR_RGB2GRAY)
+    # Grayscale
+    gray = cv2.cvtColor(img_copy,cv2.COLOR_RGB2GRAY)
 
-# Gaussian blur
-kernel_size = 5
-blur_gray = cv2.GaussianBlur(gray,(kernel_size,kernel_size),0)
+    # Gaussian blur
+    kernel_size = 5
+    blur_gray = cv2.GaussianBlur(gray,(kernel_size,kernel_size),0)
 
-# Canny
-low_thresh = 50
-high_thresh = 150
-canny_img = cv2.Canny(blur_gray,low_thresh,high_thresh) #outputs a binary image
+    # Canny
+    low_thresh = 50
+    high_thresh = 150
+    canny_img = cv2.Canny(blur_gray,low_thresh,high_thresh) # outputs a binary image
 
-# Mask
-margin = 100
-vertices = np.array([[0,y_size],[x_size/2-margin,y_size/2+margin],[x_size/2+margin,y_size/2+margin],[x_size,y_size]],np.int32)
-mask = np.zeros_like(canny_img)
-cv2.fillPoly(mask,[vertices],255)
-masked_canny_img = np.bitwise_and(canny_img,mask)
+    # Mask
+    margin = 100
+    vertices = np.array([[0,y_size],[x_size/2-margin,y_size/2+margin],[x_size/2+margin,y_size/2+margin],[x_size,y_size]],np.int32)
+    mask = np.zeros_like(canny_img)
+    cv2.fillPoly(mask,[vertices],255)
+    masked_canny_img = np.bitwise_and(canny_img,mask)
 
-# Hough lines
-rho = 2
-theta = np.pi/180
-thresh = 15
-min_line_lenth = 40
-max_gap_bet_lines = 20
-lines = cv2.HoughLinesP(masked_canny_img,rho,theta,thresh,np.array([]),min_line_lenth,max_gap_bet_lines)
-
-# Lane line detection
-left_line_xs = []
-left_line_ys = []
-right_line_xs = []
-right_line_ys  = []
-
-for line in lines:
-    x1,y1,x2,y2 = line[0]
-    slope = (y2-y1) / (x2-x1)
-    if slope < 0.:
-        left_line_xs.extend((x1,x2))
-        left_line_ys.extend((y1,y2))
-    elif slope > 0.:
-        right_line_xs.extend((x1,x2))
-        right_line_ys.extend((y1,y2))
+    # Hough lines
+    rho = 2
+    theta = np.pi/180
+    thresh = 15
+    min_line_lenth = 40
+    max_gap_bet_lines = 20
+    lines = cv2.HoughLinesP(masked_canny_img,rho,theta,thresh,np.array([]),min_line_lenth,max_gap_bet_lines)
 
 
-left_line_params = np.polyfit(left_line_xs,left_line_ys,1)
-right_line_params = np.polyfit(right_line_xs,right_line_ys,1)
+    left_line_slopes = []
+    left_line_intercepts = []
+    right_line_slopes = []
+    right_line_intercepts = []
+
+
+    for line in lines:
+        x1,y1,x2,y2 = line[0]
+        slope = (y2-y1) / (x2-x1)
+        intercept = y1 - (slope * x1)
+        if slope < 0.:
+            left_line_slopes.append(slope)
+            left_line_intercepts.append(intercept)
+        elif slope > 0.:
+            right_line_slopes.append(slope)
+            right_line_intercepts.append(intercept)
+
+
+    if len(left_line_slopes)>0 and len(left_line_intercepts)>0:
+        avg_left_line_slope = np.mean(np.array(left_line_slopes))
+        avg_left_line_intercept = np.mean(np.array(left_line_intercepts))
+
+    if len(right_line_slopes)>0 and len(right_line_intercepts)>0:
+        avg_right_line_slope = np.mean(np.array(right_line_slopes))
+        avg_right_line_intercept = np.mean(np.array(right_line_intercepts))
 
 
 
-y_min = int(y_size/2+margin)
-y_max = y_size
+    y_min = int(y_size/2+margin)
+    y_max = y_size
 
-# Draw left lane line
-left_x_min = int((y_min-left_line_params[1]) / left_line_params[0])
-left_x_max = int((y_max-left_line_params[1]) / left_line_params[0])
-cv2.line(img_copy,(left_x_min,y_min),(left_x_max,y_max),[255,0,0],10)
+    # Compute left lane line
+    left_x_min = int((y_min-avg_left_line_intercept) / avg_left_line_slope)
+    left_x_max = int((y_max-avg_left_line_intercept) / avg_left_line_slope)
+    
 
-# Draw left lane line
-right_x_min = int((y_min-right_line_params[1]) / right_line_params[0])
-right_x_max = int((y_max-right_line_params[1]) / right_line_params[0])
-cv2.line(img_copy,(right_x_min,y_min),(right_x_max,y_max),[255,0,0],10)
+    # Compute right lane line
+    right_x_min = int((y_min-avg_right_line_intercept) / avg_right_line_slope)
+    right_x_max = int((y_max-avg_right_line_intercept) / avg_right_line_slope)
 
 
-# Plot image
-plt.imshow(img_copy)
-plt.show()
+    # Create lane image
+    lane_lines_img = np.zeros_like(img_copy)
+    cv2.line(lane_lines_img,(left_x_min,y_min),(left_x_max,y_max),[0,0,255],12)
+    cv2.line(lane_lines_img,(right_x_min,y_min),(right_x_max,y_max),[0,0,255],12)
+
+
+    # Blend images
+    out_img = cv2.addWeighted(img_copy, 1, lane_lines_img, 0.8, 0)
+
+
+    return out_img
+
+
+
+# Video reader
+cap = cv2.VideoCapture('input/solidWhiteRight.mp4')
+
+# Video writer
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output/output.avi',fourcc,20.0  ,(int(cap.get(3)), int(cap.get(4))))
+
+while(cap.isOpened()):
+    ret,frame = cap.read()
+    if ret==True:
+        frame = lane_line_detection(frame)
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(25) and 0xFF==ord('q'):
+            break
+
+    else:
+        break
+
+
+# Release all
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+
         
         
 
